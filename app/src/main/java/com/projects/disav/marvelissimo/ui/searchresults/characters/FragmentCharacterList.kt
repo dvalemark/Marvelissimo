7ffb27136a2abcc6c1df7ba4ff38245ec3e69efe
dvalemark <32665808+dvalemark@users.noreply.github.com>
2019-02-04 15:35:48 +0100

    added searchQuery methods in character fragment in the onPrepareOptionsmenu, not working

--- app/src/main/java/com/projects/disav/marvelissimo/ui/searchresults/characters/FragmentCharacterList.kt
+++ app/src/main/java/com/projects/disav/marvelissimo/ui/searchresults/characters/FragmentCharacterList.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.SearchView
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import com.projects.disav.marvelissimo.R
@@ -14,7 +16,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.recyclerview.view.*
 
-class FragmentCharacterList: Fragment() {
+class FragmentCharacterList: Fragment(){
 
     private lateinit var linearLayoutManager: LinearLayoutManager
     private lateinit var adapter: RecyclerAdapterCharacter
@@ -44,5 +46,35 @@ class FragmentCharacterList: Fragment() {
 
         return view
     }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        val searchItem = menu.findItem(R.id.search)
+        if(searchItem != null){
+            val searchView = searchItem.actionView as SearchView
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String?): Boolean {
+
+                return true
+                }
+
+                override fun onQueryTextChange(newQuery: String?): Boolean {
+                    if(newQuery != null){
+                        MarvelHandler.service.getCharactersByNameStartingWith(newQuery.toLowerCase())
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe { wrapper -> adapter.characters = wrapper.data.results
+                                adapter.notifyDataSetChanged()}
+                    }
+                return true
+                }
+            })
+
+        }
+
+
+
+        super.onPrepareOptionsMenu(menu)
+    }
+
 
 }
